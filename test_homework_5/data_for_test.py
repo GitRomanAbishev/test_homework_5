3b53e994c019e3de9713c0af28f142a1edc7a10d
roman abishev <r.abishev@bi.zone>
2022-09-12 00:23:56 +0300

    Update subject and uplaod file fields

--- test_homework_5/data_for_test.py
+++ test_homework_5/data_for_test.py
@@ -1,5 +1,7 @@
+import os
 import pytest
 from selene.support.shared import browser
+
 
 
 def filling_date():
@@ -8,15 +10,14 @@ def filling_date():
     browser.element('#lastName').type('Last Name')
     browser.element('#userEmail').type('some@Email.ru')
     browser.element('#userNumber').type('7963123458')
-    browser.element('//*[@id="subjectsInput"]').type('Some Subjects')
+    browser.element('//*[@id="subjectsInput"]').type('Maths').press_enter()
     browser.element('//*[@id="currentAddress"]').type('Some address')
     browser.element('[for="gender-radio-1"').click()
     browser.element('[for="hobbies-checkbox-1"').click()
     browser.element('[for="hobbies-checkbox-2"').click()
     browser.element('[for="hobbies-checkbox-3"').click()
     browser.element('#dateOfBirthInput').click().element('//*[@class="react-datepicker__week"] [4]').click()
-    browser.element('#uploadPicture').send_keys(
-        "C:/Users/r.abishev/PycharmProjects/test_homework_5/demoqa_test/files_for_tests/for_test.txt")
+    browser.element('#uploadPicture').send_keys(os.path.abspath('files_for_tests/test_picture.png'))
     browser.element('#state').click()
     browser.element('#react-select-3-option-0').click()
     browser.element('#city').click()
--- test_homework_5/test_homework_5.py
+++ test_homework_5/test_homework_5.py
@@ -2,12 +2,13 @@ from selene import be, have
 from selene.support.shared import browser
 import pytest
 from data_for_test import filling_date
+import os
+
 
 def test_submit_date():
     filling_date()
     browser.element('#submit').click()
-    browser.element('#example-modal-sizes-title-lg').vizibale = True
+    browser.element('#example-modal-sizes-title-lg').should(be.visible)
     # А это работает вообще? я проверяю видимость элемента?
     # Все данные для заполнения описаны в функции filling_date и импортированны
     # Скажите, нормально, что я все данные ввожу в функции за приделами теста?
-
