37d626c14ef44f38496a0f4fcc2e6d3a8082efcb
roman abishev <r.abishev@bi.zone>
2022-09-13 22:47:49 +0300

    Changed the date of birth and added checks

--- test_homework_5/data_for_test.py
+++ test_homework_5/data_for_test.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from selene.support.shared import browser
-
+from selene import have, be
 
 
 def filling_date():
@@ -16,9 +16,27 @@ def filling_date():
     browser.element('[for="hobbies-checkbox-1"').click()
     browser.element('[for="hobbies-checkbox-2"').click()
     browser.element('[for="hobbies-checkbox-3"').click()
-    browser.element('#dateOfBirthInput').click().element('//*[@class="react-datepicker__week"] [4]').click()
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').type('2021')
+    browser.element('.react-datepicker__month-select').type('April')
+    browser.element('[aria-label="Choose Thursday, April 15th, 2021"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('files_for_tests/test_picture.png'))
     browser.element('#state').click()
     browser.element('#react-select-3-option-0').click()
     browser.element('#city').click()
     browser.element('#react-select-4-option-0').click()
+
+#Эту часть честно признаюсь подсмотрел, так как не разобрался, но когда посомтрел разобрался!
+def check_form_data():
+    browser.element('#example-modal-sizes-title-lg').should(be.visible).should(
+        have.text('Thanks for submitting the form'))
+    browser.element('.table-responsive').should(have.text('Name' + ' ' + 'Last Name'))
+    browser.element('.table-responsive').should(have.text('some@Email.ru'))
+    browser.element('.table-responsive').should(have.text('Male'))
+    browser.element('.table-responsive').should(have.text('7963123458'))
+    browser.element('.table-responsive').should(have.text('15 April,2021'))
+    browser.element('.table-responsive').should(have.text('Maths'))
+    browser.element('.table-responsive').should(have.text('Sports, Reading, Music'))
+    browser.element('.table-responsive').should(have.text('test_picture.png'))
+    browser.element('.table-responsive').should(have.text('Some address'))
+    browser.element('.table-responsive').should(have.text('NCR Delhi'))
--- test_homework_5/test_homework_5.py
+++ test_homework_5/test_homework_5.py
@@ -1,12 +1,16 @@
 from selene import be, have
 from selene.support.shared import browser
 import pytest
-from data_for_test import filling_date
+from data_for_test import filling_date, check_form_data
 import os
 
 
 def test_submit_date():
     filling_date()
     browser.element('#submit').click()
-    browser.element('#example-modal-sizes-title-lg').should(be.visible)
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    check_form_data()
+
+
+
 
